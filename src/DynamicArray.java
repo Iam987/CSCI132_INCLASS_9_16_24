9a497177d50f2516e1ffce83cab2da04a30fcd98
Iam987 <iancrittenden@rocketmail.com>
2024-09-16 15:49:45 -0600

    Dynamic arrays !! very usefull for the future

---
+++ src/DynamicArray.java
@@ -0,0 +1,42 @@
+
+public class DynamicArray {
+	private String[] data =  new String[0];
+	
+	public DynamicArray() {
+		
+	}
+	
+	public void add(String newValue) {
+		String[] newArray = new String[this.data.length +1];
+		for(int i = 0; i < this.data.length; i++) {
+			newArray[i] = this.data[i];
+		}
+		newArray[this.data.length] = newValue;
+		this.data = newArray;
+	}
+	
+	public void printArray() {
+		String s = "";
+		s += "[";
+		for(int i = 0; i < data.length;i++) {
+			if(i != data.length - 1) {
+			s += data[i] + " ";
+			}
+			else {
+				s += data[i];
+			}
+		}
+		s += "]";
+		System.out.println(s);
+	}
+	
+	public int find(String searchName) {
+		for(int i = 0; i < data.length; i++) {
+			if(data[i].equals(searchName)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
+
---
+++ src/DynamicArrayDemo.java
@@ -0,0 +1,18 @@
+
+public class DynamicArrayDemo {
+
+	public static void main(String[] args) {
+		DynamicArray array = new DynamicArray();
+		array.add("First");
+		array.add("Second");
+		array.add("Third");
+		array.printArray();
+
+		array.add("Fourth");
+		array.add("Fifth");
+		array.add("Sixth");
+		array.printArray();
+
+	}
+
+}
